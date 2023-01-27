2fc30cdf8807cf8c8a486bc141338c04c4146024
mgckaled <marcelgckaled@gmail.com>
2023-01-27 10:54:51 -0300

    feat: install OneSignal and add id generated to project

--- App.tsx
+++ App.tsx
@@ -1,27 +1,35 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import {
+	Roboto_400Regular,
+	Roboto_700Bold,
+	useFonts
+} from '@expo-google-fonts/roboto'
+import { NativeBaseProvider } from 'native-base'
+import { StatusBar } from 'react-native'
+import OneSignal from 'react-native-onesignal'
 
-import { Routes } from './src/routes';
+import { Routes } from './src/routes'
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { Loading } from './src/components/Loading'
+import { THEME } from './src/theme'
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import { ONE_SIGNAL_APP_ID } from './env'
+import { CartContextProvider } from './src/contexts/CartContext'
+
+OneSignal.setAppId(ONE_SIGNAL_APP_ID)
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
-  return (
-    <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <CartContextProvider>
-        {fontsLoaded ? <Routes /> : <Loading />}
-      </CartContextProvider>
-    </NativeBaseProvider>
-  );
+	return (
+		<NativeBaseProvider theme={THEME}>
+			<StatusBar
+				barStyle="light-content"
+				backgroundColor="transparent"
+				translucent
+			/>
+			<CartContextProvider>
+				{fontsLoaded ? <Routes /> : <Loading />}
+			</CartContextProvider>
+		</NativeBaseProvider>
+	)
 }
